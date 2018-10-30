b3216ffaced407d3e63fa9937716a83ac16a31e2
AaronH <a_h230@txstate.edu>
2018-10-30 16:37:01 -0500

    Added ability to delete list elements on buttonPress

--- Aaron_BranchDemo/script.js
+++ Aaron_BranchDemo/script.js
@@ -1,3 +1,96 @@
+//Create new list item when clicking on the "Add" button
+function newSubject() 
+{
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("subjectInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+//Input Validation
+  if (inputValue === '') 
+  {
+    alert("You must write something!");
+  } 
+  else 
+  {
+    li.tabIndex = 1;
+    li.className += "button button2";
+    document.getElementById("subjectUL").appendChild(li);
+  }
+  document.getElementById("subjectInput").value = "";
+}
+
+//Create new list item when clicking on the "Add" button
+function newCategory() 
+{
+  var btn = document.createElement("button");
+  var inputValue = document.getElementById("categoryInput").value;
+  var t = document.createTextNode(inputValue);
+  btn.appendChild(t);
+//Input Validation
+  if (inputValue === '') 
+  {
+    alert("You must write something!");
+  } 
+  else 
+  {
+    btn.tabIndex=2;
+    btn.className += "button button2";
+    document.getElementById("categoryDisplay").appendChild(btn);
+  }
+  document.getElementById("categoryInput").value = "";
+}
+
+
+
+$(document).ready(function()
+{
+  //on-click event for subject list
+  $("#subjectUL").on("click", "li", function()
+  {
+    $("#categoryHeader").text($(this).text());
+    $("#categoryDisplay").removeClass("second_level_hide");
+    $("#categoryDisplay").addClass("second_level");
+
+  });
+  //hiding element in subject list on double click 
+    $("#subjectUL").on("keypress", "li", function(event)
+  {
+    if (event.keyCode == 68 || event.keyCode == 100)
+    {
+      $(this).remove();      
+    }
+
+  });
+  //on-click event for category list
+  $("#categoryDisplay").on("click", "button", function()
+  {
+    $("#taskHeader").text($(this).text());
+    $("#tableDisplay").removeClass("third_level_hide");
+    $("#tableDisplay").addClass("third_level");
+  });
+
+
+//ajax row data
+	var ajax_data =
+	[
+		{fname:" ", lname:"", email:""}, 
+		{fname:"", lname:"", email:""}, 
+		{fname:"", lname:"", email:""}, 
+		{fname:"", lname:"", email:""}, 
+		{fname:"", lname:"", email:""}, 
+		{fname:"", lname:"", email:""}, 
+	]
+
+	var random_id = function  () 
+	{
+		var id_num = Math.random().toString(9).substr(2,3);
+		var id_str = Math.random().toString(36).substr(2);
+		
+		return id_num + id_str;
+	}
+
+}); 
+
 
 /*
 var list = document.querySelector('ul');
@@ -34,93 +127,3 @@ for (i = 0; i < close.length; i++)
   }
 }
 */
-
-
-
-
-//Create new list item when clicking on the "Add" button
-function newSubject() 
-{
-  alert("newSubject called");
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("subjectInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-//Input Validation
-  if (inputValue === '') 
-  {
-    alert("You must write something!");
-  } 
-  else 
-  {
-    li.className += "button button2";
-    document.getElementById("subjectUL").appendChild(li);
-  }
-  document.getElementById("subjectInput").value = "";
-}
-
-//Create new list item when clicking on the "Add" button
-function newCategory() 
-{
-  alert("newCategory called");
-  var btn = document.createElement("button");
-  var inputValue = document.getElementById("categoryInput").value;
-  var t = document.createTextNode(inputValue);
-  btn.appendChild(t);
-//Input Validation
-  if (inputValue === '') 
-  {
-    alert("You must write something!");
-  } 
-  else 
-  {
-    btn.className += "button button2";
-    document.getElementById("categoryDisplay").appendChild(btn);
-  }
-  document.getElementById("categoryInput").value = "";
-}
-
-
-
-$(document).ready(function()
-{
-  //on-click even for subject list
-  $("#subjectUL").on("click", "li", function()
-  {
-    $(this).hide(1000);
-    $(this).show(1000);
-    $("#categoryHeader").text($(this).text());
-    $("#categoryDisplay").removeClass("second_level_hide");
-    $("#categoryDisplay").addClass("second_level");
-
-  });
-
-  $("#categoryDisplay").on("click", "button", function()
-  {
-    $("#taskHeader").text($(this).text());
-    $("#tableDisplay").removeClass("third_level_hide");
-    $("#tableDisplay").addClass("third_level");
-
-  });
-
-//ajax row data
-	var ajax_data =
-	[
-		{fname:" ", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-	]
-
-	var random_id = function  () 
-	{
-		var id_num = Math.random().toString(9).substr(2,3);
-		var id_str = Math.random().toString(36).substr(2);
-		
-		return id_num + id_str;
-	}
-
-}); 
-
