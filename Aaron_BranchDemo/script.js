577c557159e479c5580aa9167f8f085d69e07ddf
AaronH <a_h230@txstate.edu>
2018-11-27 16:34:26 -0600

    Implemented Dynamic Show/Hide on 2nd Level

--- Aaron_BranchDemo/script.js
+++ Aaron_BranchDemo/script.js
@@ -59,10 +59,10 @@ function newCategory()
       categoryText: inputValue, //Name given to button by user input
       subject: subjectName //Name of associated subject
     }
-    var t = document.createTextNode(inputValue)
+    var t = document.createTextNode(inputValue);
     document.getElementById("categoryInput").value = "";
     subjectName = $("#categoryHeader").text();
-    categoryObject.categoryItem.appendChild(t)
+    categoryObject.categoryItem.appendChild(t);
     categoryObject.categoryItem.tabIndex=2;
     categoryObject.categoryItem.className += "button button2";
     document.getElementById("categoryDisplay").appendChild(categoryObject.categoryItem);
@@ -74,6 +74,7 @@ function newCategory()
       if (subjectList[x].subjectText == subjectName)
       {
         subjectList[x].categoryList.push(categoryObject);
+        //alert("subjectList[x].categoryList: " + categoryObject.categoryItem.text());
       }
     }
   }
@@ -91,68 +92,50 @@ $(document).ready(function()
   //*********************************************************************
   $("#subjectButtons").on("click", "button", function()
   {
+  	$("#categoryHeader").text($(this).text());
+    $("#categoryDisplay").removeClass("second_level_hide");
+    $("#categoryDisplay").addClass("second_level");
+
     subjectName = $("#categoryHeader").text();
     var showIndex;
     //***************************************
     // Toggle Show/Hide 
     //***************************************
+    alert (subjectList.length);
+
     for (i = 0; i < subjectList.length; i++)
     {
       if (subjectList[i].subjectText == subjectName)
       {
+      	alert ("catList Length: " + subjectList[i].categoryList.length);
+
+      	//If Matched, Show List Items
+        showIndex = i;
+
         for (j = 0; j < subjectList[i].categoryList.length; j++)
         {
-          subjectList[i].categoryList[j].categoryItem.removeClass("second_level_hide");
-          subjectList[i].categoryList[j].categoryItem.addClass("second_level");
-        }
-      }
-      else (j = 0; j < subjectList[i].categoryList.length; j++)
-      {
-        for (j = 0; j < subjectList[i].categoryList.length; j++)
-        {
-          subjectList[i].categoryList[j].categoryItem.removeClass("second_level_hide");
-          subjectList[i].categoryList[j].categoryItem.addClass("second_level");
-        }
-      }
-    }
-
-
-
-
-
-
-
-
-        $.each(subjectList[i].categoryList, function() 
-        {
-          for (p = 0; p < subjectList[i].categoryList.length; p++)
+        	alert ("show: " + subjectList[i].categoryList[j].categoryItem.innerHTML);
+          $("li").filter(function()
           {
-            if (showIndex = p)
+            return $(this).text() == subjectList[i].categoryList[j].categoryItem.innerHTML;
+          }).show();
+        }
+      }
+      else 
+      {
+      	//If not Matched, Hide Items
+        for (j = 0; j < subjectList[i].categoryList.length; j++)
+        {
+          alert ("hide: " + subjectList[i].categoryList[j].categoryItem.innerHTML);
+          $("li").filter(function()
             {
-              //Change css of this particular button
-              //Can maybe do it with index?
-              var item1 = $( "li" )[ index ];
-              shownIndexes.push(index);
-              $(item1).removeClass("button button2");
-              $(item1).addClass("second_level_hide");
-              $( "li" ).each(function( index2 ) 
-              {
-                if (!shownIndexes.includes(index2))
-                {
-                  var item2 = $( "li" )[ index2 ];
-                  $(item2).removeClass("second_level_hide");
-                  $(item2).addClass("button button2");
-                  //hiddenIndexes.push(index2);
-                }
-              });
-            }
-          }
-        });
+              return $(this).text() == subjectList[i].categoryList[j].categoryItem.innerHTML;
+            }).hide();
+        }
       }
     }
-    $("#categoryHeader").text($(this).text());
-    $("#categoryDisplay").removeClass("second_level_hide");
-    $("#categoryDisplay").addClass("second_level");
+
+
   });
 
   //*********************************************************************
@@ -170,9 +153,9 @@ $(document).ready(function()
   // When user clicks a button within the second level, the third level 
   // will be displayed and header assigned to text of button.
   //*********************************************************************
-  $("#categoryDisplay").on("click", "button", function()
+  $("#categoryDisplay").on("click", "li", function()
   {
-    $("#taskHeader").text($(this).text());
+    $("#tableHeader").text($(this).text());
     $("#tableDisplay").removeClass("third_level_hide");
     $("#tableDisplay").addClass("third_level");
   });
