fe250750fa1cc5c624aee9dd8dca45dc07253ef7
Mary <mjg128@txstate.edu>
2018-12-03 09:20:44 -0600

    small table changes

--- demo/script.js
+++ demo/script.js
@@ -70,17 +70,6 @@ $(document).ready(function()
   });
 
 
-//ajax row data
-	var ajax_data =
-	[
-		{fname:" ", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-		{fname:"", lname:"", email:""}, 
-	]
-
 	var random_id = function  () 
 	{
 		var id_num = Math.random().toString(9).substr(2,3);
@@ -89,39 +78,46 @@ $(document).ready(function()
 		return id_num + id_str;
 	}
 
-}); 
+	}); 
+	
+function generate_table() {
+	
+	
+	
+  // get the reference for the body
+  var body = document.getElementsByTagName("body")[0];
+  // creates a <table> element and a <tbody> element
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  // creating all cells
+  for (var i = 0; i < 3; i++) {
+    // creates a table row
+    var row = document.createElement("tr");
+
+    for (var j = 0; j < 3; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      var cell = document.createElement("td");
+	  
+      var cellText = document.createTextNode("edit me").contentEditable="true";
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "1");
+    document.getElementById("tableDisplay").appendChild(tbl);
+
+}
 
 
-/*
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) 
-{
-  if (ev.target.tagName === 'LI') 
-  {
-    //document.getElementById("categoryHeader").innerHTML = ev.target.innerHTML;
-    alert("event processed");
-  }
-}, false);
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) 
-{
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) 
-{
-  close[i].onclick = function() 
-  {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-*/
