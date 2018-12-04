75e8b3549525634798591ea0965c101d7241e0e8
j-c545 <42955673+j-c545@users.noreply.github.com>
2018-12-04 17:30:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -37,17 +37,10 @@ function newSubject()
       }
       //Appending new subject object to global subjectList list
       subjectList.push(subjectObject);
-	  
+      alert("Subject created for: " + subjectObject.subjectText);
   }
 }
 
-
-//*********************************************************************
-// newCategory() dynamically creates a new category button and
-// a category object that is associated with the subject
-// selected on the first level.
-//*********************************************************************
-function newCategory() {
 //*********************************************************************
 // newCategory() dynamically creates a new category button and
 // a category object that is associated with the subject
@@ -68,6 +61,115 @@ function newCategory()
       subject: subjectName //Name of associated subject
 
     }
+    alert("Category created for: " + categoryObject.categoryText);
+    
+    //Create Table and Caption
+    var cTable = document.createElement("TABLE");
+    cTable.border = "1";
+    cTable.id = categoryObject.categoryText;
+    var cap = cTable.createCaption();
+    cap.innerHTML = "<b>" + categoryObject.categoryText + "</b>";
+
+    //Add the header row.
+    var row = cTable.insertRow(-1);
+    var header1 = document.createElement("TH");
+    header1.innerHTML = "Date";
+    row.appendChild(header1);
+    var header2 = document.createElement("TH");
+    header2.innerHTML = "Event";
+    row.appendChild(header2);
+    var header3 = document.createElement("TH");
+    header3.innerHTML = "Description";
+    row.appendChild(header3);
+
+    //Add data rows
+    for (var i = 1; i < 4; i++)
+    {
+
+        var row = cTable.insertRow(i);
+        row.id = cTable.id+","+"row"+i;
+
+        var cell1 = row.insertCell(0);
+        cell1.contentEditable = true;
+        cell1.id = cTable.id+","+"name_row"+i;
+
+        var cell2 = row.insertCell(1);
+        cell2.contentEditable = true;
+        cell2.id = cTable.id+","+"country_row"+i;
+
+        var cell3 = row.insertCell(2);
+        cell3.contentEditable = true;
+        cell3.id = cTable.id+","+"age_row"+i;
+
+        var inputCell = row.insertCell(3);
+        var deleteButton = document.createElement("button");
+        deleteButton.id = cTable.id+","+"delete_button"+i;
+        deleteButton.className = "delete";
+        var t = document.createTextNode("Delete");
+        deleteButton.appendChild(t);
+        //deleteButton.onclick = function(){delete_row(i)};
+        inputCell.append(deleteButton);
+
+        var editButton = document.createElement("button");
+        editButton.id = cTable.id+","+"edit_button"+i;
+        editButton.className = "edit";
+        var t = document.createTextNode("Edit");
+        editButton.appendChild(t);
+        //editButton.onclick = function(){edit_row(i)};
+        inputCell.append(editButton);
+
+        var saveButton = document.createElement("button");
+        saveButton.id = cTable.id+","+"save_button"+i;
+        saveButton.className = "save";
+        var t = document.createTextNode("Save");
+        saveButton.appendChild(t);
+        //saveButton.onclick = function(){save_row(i)};
+        inputCell.append(saveButton);
+
+
+    }
+    row = cTable.insertRow(4);
+    var cell1 = row.insertCell(0);
+    cell1.contentEditable = true;
+    cell1.id = cTable.id+"new_name";
+    var cell2 = row.insertCell(1);
+    cell2.contentEditable = true;
+    cell2.id = cTable.id+"new_country";
+    var cell3 = row.insertCell(2);
+    cell3.contentEditable = true;
+    cell3.id = cTable.id+"new_age";
+
+    var cell4 = row.insertCell(3);
+    var addButton = document.createElement("button");
+    addButton.className = "addButton";
+    var t = document.createTextNode("Add Row");
+    //addButton.onclick = function(){add_row()};
+    addButton.appendChild(t);
+    cell4.append(addButton);
+
+    //Add Table to Document
+    document.getElementById("tableDisplay").append(cTable);
+
+    var t = document.createTextNode(inputValue);
+    document.getElementById("categoryInput").value = "";
+    subjectName = $("#categoryHeader").text();
+    categoryObject.categoryItem.appendChild(t);
+    categoryObject.categoryItem.tabIndex=2;
+    categoryObject.categoryItem.className += "button button2";
+    document.getElementById("categoryDisplay").appendChild(categoryObject.categoryItem);
+    
+    //Looping through global subject list, matching the category to
+    //the subject, appending category object to the subject's categoryList
+    for (x = 0; x < subjectList.length; x++)
+    {
+      if (subjectList[x].subjectText == subjectName)
+      {
+        subjectList[x].categoryList.push(categoryObject);
+		
+        //alert("subjectList[x].categoryList: " + categoryObject.categoryItem.text());
+      }
+    }
+  }
 }
 
 //*********************************************************************
@@ -226,3 +328,14 @@ $(document).ready(function()
   });
 
 
+  //   $(".delete").on("click", "button", function ()
+  // {
+  //   alert("delete found");
+  //   $(this).parents("tr").remove();
+  // });
+
+
+
+
+//END OF JQUERY 
+}); 
