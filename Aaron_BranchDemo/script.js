2e13ca1de3807fc12ee1d81a590474fc83f6706d
AaronH <a_h230@txstate.edu>
2018-12-04 12:51:13 -0600

    changed delete keypress to F from D
    in compliance with NVDA shortcuts

--- Aaron_BranchDemo/script.js
+++ Aaron_BranchDemo/script.js
@@ -194,8 +194,6 @@ $(document).ready(function()
     //***************************************
     // Toggle Show/Hide 
     //***************************************
-   // alert (subjectList.length);
-
     for (i = 0; i < subjectList.length; i++)
     {
       if (subjectList[i].subjectText == subjectName)
@@ -233,7 +231,7 @@ $(document).ready(function()
   // subjectButtons, that button will be removed.
   //*********************************************************************
   $("#subjectButtons").on("keypress", "button", function(event){
-    if (event.keyCode == 68 || event.keyCode == 100)
+    if (event.keyCode == 70 || event.keyCode == 102)
     {
       if (confirm ('Press OK to delete subject: ' + $(this).text()))
       {
@@ -270,7 +268,7 @@ $(document).ready(function()
   });
 
  $("#categoryDisplay").on("keypress", "li", function(event){
-    if (event.keyCode == 68 || event.keyCode == 100)
+    if (event.keyCode == 70 || event.keyCode == 102)
     {
       if (confirm ('Press OK to delete category: ' + $(this).text()))
       {
